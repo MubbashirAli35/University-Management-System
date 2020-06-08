ede7641f1d4bb3000ddd7e61080847b4ca36fb8a
Mubbashir Ali <mubbashirali35@gmail.com>
2020-06-09 03:20:27 +0500

    Background Image added (second commit)

---
+++ src/components/BackgroundImageComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BackgroundImage(props) {
+    return(
+        <img src="background_image.jpg"  width='1000px' height='2000px'/>
+    );
+}
+
+export default BackgroundImage;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import NavbarComponent from './NavbarComponent';
+import BackgroundImage from './BackgroundImageComponent';
+import { Media } from 'reactstrap';
 
 function HomeComponent(props) {
 
     return(
-        <NavbarComponent />
+        <div>
+            <NavbarComponent />
+            <img src={require('./background_image.jpg')} className='bg-image' />
+        </div>
     );
 }
 
